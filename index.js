f7ebd0013b9af691170ea4976be3c98168613a2f
Rafael Oliveira <rafael.oliveira@culturainglesa.com.br>
2023-04-15 15:51:11 -0300

    Added tray icon

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
+const path = require('path');
 const electron = require('electron');
 
-const { app, BrowserWindow } = electron;
+const { app, BrowserWindow, Tray } = electron;
 
 let mainWindow;
 
@@ -16,5 +17,10 @@ app.on('ready', () => {
     }
   });
 
+  const iconName = process.platform === 'win32' ? 'windows-icon.png' : 'iconTemplate.png'
+  const iconPath = path.join(__dirname, `./src/assets/${iconName}`);
+
   mainWindow.loadURL(`file://${__dirname}/src/index.html`);
+
+  new Tray(iconPath);
 });
